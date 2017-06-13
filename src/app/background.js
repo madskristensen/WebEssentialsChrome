2b33e19e25dee04a2c7413fbb41dee4d9f4187aa
Mads Kristensen <post@madskristensen.net>
2017-06-13 16:16:48 -0700

    Added missing semicolon

--- src/app/background.js
+++ src/app/background.js
@@ -4,7 +4,7 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     if (request.type === "extensionlist" && request.data.length > 0) {
 
         chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            data[tabs[0].id] = request.data
+            data[tabs[0].id] = request.data;
             chrome.pageAction.show(tabs[0].id);
         });
     }
