912e87e477bddf3ba255acd5cba4428bb3de7bc6
Mads Kristensen <post@madskristensen.net>
2017-06-13 16:06:24 -0700

    Updates to support Firefox

--- src/app/background.js
+++ src/app/background.js
@@ -1,11 +1,11 @@
 var data = {};
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.type === "extensionlist" && request.data.length > 0) {
+    if (request.type === "extensionlist" && request.data.length > 0) {
 
-    chrome.tabs.getSelected(null, function (tab) {
-      data[tab.id] = request.data
-      chrome.pageAction.show(tab.id);
-    });
-  }
+        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+            data[tabs[0].id] = request.data
+            chrome.pageAction.show(tabs[0].id);
+        });
+    }
 });
--- src/app/popup.js
+++ src/app/popup.js
@@ -3,53 +3,53 @@ let bgp = chrome.extension.getBackgroundPage();
 
 function showList(extensions) {
 
-  for (let i = 0; i < extensions.length; i++) {
+    for (let i = 0; i < extensions.length; i++) {
 
-    let items = extensions[i].menu;
+        let items = extensions[i].menu;
 
-    let header = getHeader(items, list);
+        let header = getHeader(items, list);
 
-    let ul = document.createElement("ul");
-    list.appendChild(ul);
+        let ul = document.createElement("ul");
+        list.appendChild(ul);
 
-    for (var item in items) {
-      if (item === "displayText")
-        continue;
+        for (var item in items) {
+            if (item === "displayText")
+                continue;
 
-      let li = document.createElement("li");
-      let a = document.createElement("a");
-      a.innerHTML = item;
-      a.href = "#";
-      a["data-callback"] = items[item];
-      a.onclick = function () { execute(extensions[i].name, this["data-callback"]); };
+            let li = document.createElement("li");
+            let a = document.createElement("a");
+            a.innerHTML = item;
+            a.href = "#";
+            a["data-callback"] = items[item];
+            a.onclick = function () { execute(extensions[i].name, this["data-callback"]); };
 
-      li.appendChild(a);
-      ul.appendChild(li);
+            li.appendChild(a);
+            ul.appendChild(li);
+        }
     }
-  }
 }
 
 function getHeader(items, list) {
 
-  var header = list.querySelector("h2[data-name='" + items.displayText + "']");
-  if (!header) {
-    header = document.createElement("h2");
-    header.innerHTML = items.displayText;
-    header.className = "header";
-    header.setAttribute("data-name", items.displayText);
-    list.appendChild(header);
-  }
+    var header = list.querySelector("h2[data-name='" + items.displayText + "']");
+    if (!header) {
+        header = document.createElement("h2");
+        header.innerHTML = items.displayText;
+        header.className = "header";
+        header.setAttribute("data-name", items.displayText);
+        list.appendChild(header);
+    }
 
-  return header;
+    return header;
 }
 
 function execute(extName, method) {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.tabs.executeScript(tabs[0].id, { code: `__bl_execute('${extName}', '${method}')`, runAt: "document_end" }, function (response) { });
-    window.close();
-  });
+    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        chrome.tabs.executeScript(tabs[0].id, { code: `__bl_execute('${extName}', '${method}')`, runAt: "document_end" }, function (response) { });
+        window.close();
+    });
 }
 
-chrome.tabs.getSelected(null, function (tab) {
-  showList(bgp.data[tab.id]);
+chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+    showList(bgp.data[tabs[0].id]);
 });
